579f688f3d36cf16749377fbe2aa44ec87d77e90
Sandra Mavsar <55741648+SandraMavsar@users.noreply.github.com>
2023-06-20 13:01:29 +0200

    Update solution.py

--- 2022/Day_10/solution.py
+++ 2022/Day_10/solution.py
@@ -1,1 +1,36 @@
 #--- Day 10: Cathode-Ray Tube ---
+with open('input.txt') as f:
+    move = f.read().strip().split("\n")
+
+def check(cycle, result):
+    if cycle in cycles:
+        result[:] = [result[0] + (cycle * x)]
+        return result
+
+def draw(crt):
+    if cycle in crt_lines:
+        print(crt)
+        crt[:] = []
+    elif len(crt) == x-1 or len(crt) == x or len(crt) == x+1:
+        crt.append('#')
+    else:
+        crt.append(' ')
+    return crt
+
+x = 1; cycle = 0; result = [0]; cycles = [20, 60, 100, 140, 180, 220, 280]
+crt_lines = [40, 80, 120, 160, 200, 240]; crt = []
+
+for i in move:
+    add = 0
+    if i[0] == 'a':
+        add = int(i[5:])
+        cycle += 1
+        draw(crt), check(cycle, result)
+        cycle += 1
+        draw(crt), check(cycle, result)
+    else:
+        cycle += 1
+        draw(crt), check(cycle, result)
+    x += add
+
+print('Part One: ', result[0])

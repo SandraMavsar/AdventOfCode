307b4e56f2ee8cc5345246122bd029f6b27d48be
DESKTOP-FFIH254\sandra <mavsar.sandra@gmail.com>
2023-12-19 13:21:21 +0100

    day 14 part one

---
+++ 2023/Day_14/solution.py
@@ -0,0 +1,35 @@
+# --- Day 14: Parabolic Reflector Dish ---
+
+
+with open('input.txt') as f:
+    lines = f.read().strip().split('\n')
+
+    map_rows = [list(line) for line in lines]
+    map_columns = list(zip(*map_rows))
+
+    for i in range(0, 100):
+        def move_zeros_left(column):
+            moved_column = list(column)
+            for i in range(len(moved_column)):
+                if moved_column[i] == 'O':
+                    for j in range(i - 1, -1, -1):
+                        if moved_column[j] == '#':
+                            break
+                        if moved_column[j] == '.':
+                            moved_column[j], moved_column[j + 1] = moved_column[j + 1], moved_column[j]
+            return tuple(moved_column)
+
+        map_columns_north = [move_zeros_left(column) for column in map_columns]
+
+    result = 0
+    for column in map_columns_north:
+        for index, character in enumerate(column):
+            if character == "O":
+                result += len(column) - index
+
+    print("Part One: ", result)
+
+
+
+
+
